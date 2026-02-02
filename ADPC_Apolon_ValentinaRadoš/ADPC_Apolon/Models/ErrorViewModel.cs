d3df5a7c9fc9c203ce1e4b549df99ba21893a05f
ValentinaRadošAlgebra <vrados@algebra.hr>
2026-02-02 20:02:48 +0100

    first commit

---
+++ ADPC_Apolon_ValentinaRadoš/ADPC_Apolon/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ADPC_Apolon.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
